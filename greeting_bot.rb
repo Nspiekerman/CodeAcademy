ab70ac6db85996731f70abb347ef75c058f9efc9
Nspiekerman <61211165+Nspiekerman@users.noreply.github.com>
2021-03-26 09:28:16 -0700

    a
    ew

---
+++ greeting_bot.rb
@@ -0,0 +1,9 @@
+puts 'Hello There, and what\'s your  first name?'
+first_name = gets.chomp
+puts 'OK, and what is your last name?'
+last_name = gets.chomp
+puts 'OK, your name is ' +  first_name + ' ' + last_name + '?'
+puts 'Nice to meet you ' +  first_name + ' ' + last_name + ' =D '
+puts 'What wwould you feel like eating todau?'
+food1 = gets.chomp
+puts food1 + ' EWWWW that sounds  gross!! ' + first_name
---
+++ number_bot.rb
@@ -0,0 +1,4 @@
+# Write a program that asks a person for their favorite number. Add  1 to  that number and return it to the user  and a bigger  and better  favorite number
+
+
+
