8e2d6c989612817e7759cd8c973a2441f66a51e1
Nspiekerman <61211165+Nspiekerman@users.noreply.github.com>
2021-03-27 10:29:02 -0700

    Addin my work so far

---
+++ Branch_practice.rb
@@ -0,0 +1,8 @@
+puts "hello, what is your name?"
+name = gets.chomp
+puts "Hello #{name}"    
+
+if name == "nick" || name == "Nick"
+    puts "Nice name!"
+
+end
---
+++ angry_boss.rb
@@ -0,0 +1,13 @@
+=begin This project, I will create and angry boss program that will rudely ask what I want and yell at you and fire you
+=end
+
+#Start
+
+puts "HEY YOU! GET IN HERE! What is your name?"
+name = gets.chomp
+puts "#{name}, I don't like that name, what do you want?"
+request = gets.chomp
+puts "#{request.capitalize}? Don't be rediculous"
+if request == "A raise"
+    puts = "#{request.capitalize}???? WHADDAYA MEAN #{request.capitalize}?!?! YOU'RE FIRED!!"
+end
